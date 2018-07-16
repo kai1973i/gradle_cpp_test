84c3018ca60045b66751208179136ac4729b0203
kai1973i <kai1973i@googlemail.com>
2018-07-16 02:14:52 +0200

    my first commit

---
+++ src/main/cpp/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "greeting.hpp"
+#include "../third/hello.hpp"
+
+int main(int argc, char** argv) {
+  std::cout << greeting << test << std::endl;
+  return 0;
+}
---
+++ src/main/third/hello.hpp
@@ -0,0 +1,3 @@
+namespace {
+  const char * test = "test";
+}
